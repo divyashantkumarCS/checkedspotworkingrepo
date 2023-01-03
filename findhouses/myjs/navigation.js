8c52e4e8d874d999217478619734a81114a4cb77
divyashantkumarCS <divyashant@checkedspot.com>
2023-01-03 19:01:14 +0530

    3-Jan

---
+++ findhouses/myjs/navigation.js
@@ -0,0 +1,53 @@
+
+const buy = document.getElementById("buy");
+const buyOptions = document.getElementById("buyOptionsDisplay");
+
+buy.addEventListener("mouseover", () => {buyOptions.style.display = "block"});
+buy.addEventListener("mouseleave", () => {
+    buyOptions.addEventListener("mouseover", () => {
+        buyOptions.style.display = "block";
+    });
+
+    buyOptions.addEventListener("mouseleave", () => {
+        buyOptions.style.display = "none";
+    });
+
+    buyOptions.style.display = "none"
+})
+
+
+
+const rent = document.getElementById("rent");
+const rentOptions = document.getElementById("rentOptionsDisplay");
+
+rent.addEventListener("mouseover", () => {rentOptions.style.display = "block"});
+rent.addEventListener("mouseleave", () => {
+    rentOptions.addEventListener("mouseover", () => {
+        rentOptions.style.display = "block";
+    });
+
+    rentOptions.addEventListener("mouseleave", () => {
+        rentOptions.style.display = "none";
+    });
+
+    rentOptions.style.display = "none"
+})
+
+
+const sell = document.getElementById("sell");
+const sellOptions = document.getElementById("sellOptionsDisplay");
+
+sell.addEventListener("mouseover", () => {sellOptions.style.display = "block"});
+sell.addEventListener("mouseleave", () => {
+    sellOptions.addEventListener("mouseover", () => {
+        sellOptions.style.display = "block";
+    });
+
+    sellOptions.addEventListener("mouseleave", () => {
+        sellOptions.style.display = "none";
+    });
+
+    sellOptions.style.display = "none"
+})
+
+
